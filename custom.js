b2205b2e92cc287e36e737bd87046954d1da9503
[Github <[Github>
2022-01-09 21:47:58 +0900

    initial commit

--- custom.js
+++ custom.js
@@ -1,9 +1,10 @@
 gsap.from('.add-memo1',{opacity:0,duration:1,y:-50});
-gsap.from('.add-memo2',{opacity:0,duration:1,y:-50});
-gsap.from('.add-memo3',{opacity:0,duration:1,y:-50});
-gsap.from('.add-memo4',{opacity:0,duration:1,y:-50});
-gsap.from('.title',{opacity:0,duration:1,y:-30});
+gsap.from('.add-memo2',{opacity:0,duration:1,y:-50,delay:0.1});
+gsap.from('.add-memo3',{opacity:0,duration:1,y:-50,delay:0.2});
+gsap.from('.add-memo4',{opacity:0,duration:1,y:-50,delay:0.3});
+gsap.from('.title',{opacity:0,duration:1,x:-50,delay:1});
 gsap.from('.add-memo button',{opacity:0,duration:3,rotate:220});
+gsap.from('.simpson',{opacity:0,ease:'bounce',duration:2,y:100});
 
 
 
@@ -12,12 +13,19 @@ function box_1_Click(bt){
   gsap.to('.text_box1',{duration:1,width:200,height:200,});
   gsap.to('.text_box1',{duration:1,width:400,delay:1});
 
+  
+
 
 }
+
+
+
+
 function box_2_Click(bt){
   gsap.to('.text_box2',{duration:4,x:300,backgroundColor:'pink',rotate:360,ease:'elastic'});
   gsap.to('.text_box2',{duration:1,width:200,height:200,});
   gsap.to('.text_box2',{duration:1,width:400,delay:1});
+ 
 
 
 }
@@ -25,6 +33,7 @@ function box_3_Click(bt){
   gsap.to('.text_box3',{duration:4,x:300,backgroundColor:'blue',rotate:360,ease:'elastic'});
   gsap.to('.text_box3',{duration:1,width:200,height:200,});
   gsap.to('.text_box3',{duration:1,width:400,delay:1});
+  
 
 
 }
@@ -32,6 +41,7 @@ function box_4_Click(bt){
   gsap.to('.text_box4',{duration:4,x:300,backgroundColor:'orange',rotate:360,ease:'elastic'});
   gsap.to('.text_box4',{duration:1,width:200,height:200,});
   gsap.to('.text_box4',{duration:1,width:400,delay:1});
+  
 
 
 }
