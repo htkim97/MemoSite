e4d2bfcf5f45121bd11b8c8e221fbd78ce48391a
[Github <[Github>
2022-01-17 20:16:07 +0900

    initial commit

--- custom.js
+++ custom.js
@@ -2,9 +2,15 @@ gsap.from('#plus_red',{opacity:0,duration:1,y:-50, rotate:220});
 gsap.from('#plus_pink',{opacity:0,duration:1,y:-50,delay:0.1, rotate:220});
 gsap.from('#plus_blue',{opacity:0,duration:1,y:-50,delay:0.2, rotate:220});
 gsap.from('#plus_orange',{opacity:0,duration:1,y:-50,delay:0.3, rotate:220});
-gsap.from('.hr1',{opacity:0,duration:1,y:50,delay:0.7});
+
+
+
+gsap.from('.hr1',{opacity:0,duration:1,y:50,delay:0.1});
 gsap.from('.title',{opacity:0,duration:1,x:-50,delay:1});
+gsap.from('.name',{opacity:0,duration:1,y:50,delay:1.5});
+
 gsap.from('.logo',{opacity:0,ease:'bounce',duration:2,x:30});
+gsap.from('#chat',{duration:3,opacity:0,ease:'elastic',duration:2,y:20, delay:2, });
  
 
 
@@ -13,40 +19,44 @@ gsap.from('.logo',{opacity:0,ease:'bounce',duration:2,x:30});
 const box_1_Click=()=>{
   const add_memo1=document.getElementById("add_memo");
   const newD=document.createElement('div');
-  newD.innerHTML="  <input type='text' id='text_box1'> <button class='material-icons' id='delete_1' onclick='remove(this)'>delete</button>";
-  add_memo1.appendChild(newD);
+  newD.innerHTML="  <textarea id='text_box1'></textarea> <button class='material-icons' id='delete_1' onclick='remove(this)'>delete</button><button class='material-icons' id='reset_1' onclick='reset_1(this)'>cached</button>";
+  add_memo.appendChild(newD);
   gsap.to('#text_box1',{ease:'elastic',duration:2,y:50});
   gsap.to('#delete_1',{ease:'elastic',duration:2,y:50});
+  gsap.to('#reset_1',{ease:'elastic',duration:2,y:50});
   
  
  
 }
 
 const box_2_Click=()=>{
-  const add_memo2=document.getElementById("add_memo");
+  const add_memo=document.getElementById("add_memo");
   const newD=document.createElement('div');
-  newD.innerHTML=" <input type='text' id='text_box2'> <button class='material-icons' id='delete_2' onclick='remove(this)'>delete</button>";
-  add_memo2.appendChild(newD);
+  newD.innerHTML=" <textarea id='text_box2'></textarea> <button class='material-icons' id='delete_2' onclick='remove(this)'>delete</button><button class='material-icons' id='reset_2' onclick='reset_2(this)'>cached</button>";
+  add_memo.appendChild(newD);
   gsap.to('#text_box2',{ease:'elastic',duration:2,y:50});
   gsap.to('#delete_2',{ease:'elastic',duration:2,y:50});
+  gsap.to('#reset_2',{ease:'elastic',duration:2,y:50});
 }
 
 const box_3_Click=()=>{
-  const add_memo3=document.getElementById("add_memo");
+  const add_memo=document.getElementById("add_memo");
   const newD=document.createElement('div');
-  newD.innerHTML=" <input type='text' id='text_box3'> <button class='material-icons' id='delete_3' onclick='remove(this)'>delete</button> ";
-  add_memo3.appendChild(newD);
+  newD.innerHTML=" <textarea id='text_box3'></textarea> <button class='material-icons' id='delete_3' onclick='remove(this)'>delete</button><button class='material-icons' id='reset_3' onclick='reset_3(this)'>cached</button> ";
+  add_memo.appendChild(newD);
   gsap.to('#text_box3',{ease:'elastic',duration:2,y:50});
   gsap.to('#delete_3',{ease:'elastic',duration:2,y:50});
+  gsap.to('#reset_3',{ease:'elastic',duration:2,y:50});
 }
 
 const box_4_Click=()=>{
-  const add_memo4=document.getElementById("add_memo");
+  const add_memo=document.getElementById("add_memo");
   const newD=document.createElement('div');
-  newD.innerHTML=" <input type='text' id='text_box4'> <button class='material-icons' id='delete_4' onclick='remove(this)'>delete</button> ";
-  add_memo4.appendChild(newD);
+  newD.innerHTML=" <textarea id='text_box4'></textarea> <button class='material-icons' id='delete_4' onclick='remove(this)'>delete</button><button class='material-icons' id='reset_4' onclick='reset_4(this)'>cached</button> ";
+  add_memo.appendChild(newD);
   gsap.to('#text_box4',{ease:'elastic',duration:2,y:50});
   gsap.to('#delete_4',{ease:'elastic',duration:2,y:50});
+  gsap.to('#reset_4',{ease:'elastic',duration:2,y:50});
 }
 
 
@@ -54,67 +64,34 @@ const box_4_Click=()=>{
 // 요소 삭제 버튼
 const remove = (obj) => {
   document.getElementById('add_memo').removeChild(obj.parentNode);
+ 
 }
 
 
-// 뿌직
+// 검색 버튼
 
-
-// function box_1_Click(bt){
-//   gsap.to('#text_box1',{duration:4,x:300,backgroundColor:'red',rotate:360,ease:'elastic'});
-//   gsap.to('#text_box1',{duration:1,width:200,height:200,});
-//   gsap.to('#text_box1',{duration:1,width:400,delay:1});
+function search(){
   
-//   gsap.to('#delete_red',{duration:1,x:330,y:-90,rotate:360,opacity:0.7, color:'red',delay:1})
- 
+  console.log("응 안되 검색");
+}
 
 
-// }
 
-// function box_2_Click(bt){
-//   gsap.to('#text_box2',{duration:4,x:300,backgroundColor:'pink',rotate:360,ease:'elastic'});
-//   gsap.to('#text_box2',{duration:1,width:200,height:200,});
-//   gsap.to('#text_box2',{duration:1,width:400,delay:1});
-//   gsap.to('#delete_pink',{duration:1,x:330,y:-60,rotate:360,opacity:0.7, color:'pink',delay:1})
- 
+// 내용 리셋
 
-
-// }
-// function box_3_Click(bt){
-//   gsap.to('#text_box3',{duration:4,x:300,backgroundColor:'blue',rotate:360,ease:'elastic'});
-//   gsap.to('#text_box3',{duration:1,width:200,height:200,});
-//   gsap.to('#text_box3',{duration:1,width:400,delay:1});
-//   gsap.to('#delete_blue',{duration:1,x:330,y:-60,rotate:360,opacity:0.7, color:'blue',delay:1})
+function reset_1(){
+  document.getElementById("text_box1").value='';
+}
   
+function reset_2(){
+  document.getElementById("text_box2").value='';
+}
 
+function reset_3(){
+  document.getElementById("text_box3").value='';
+}
 
-// }
-// function box_4_Click(bt){
-//   gsap.to('#text_box4',{duration:4,x:300,backgroundColor:'orange',rotate:360,ease:'elastic'});
-//   gsap.to('#text_box4',{duration:1,width:200,height:200,});
-//   gsap.to('#text_box4',{duration:1,width:400,delay:1});
-//   gsap.to('#delete_orange',{duration:1,x:330,y:-60,rotate:360,opacity:0.7, color:'orange',delay:1})
-  
-
-
-// }
-
-
-// // 뿌직2 (input 내용 초기화)
-
-// function delete_1_click(){
-//   document.getElementById("text_box1").value='';
-// }
-  
-// function delete_2_click(){
-//   document.getElementById("text_box2").value='';
-// }
-
-// function delete_3_click(){
-//   document.getElementById("text_box3").value='';
-// }
-
-// function delete_4_click(){
-//   document.getElementById("text_box4").value='';
-// }
+function reset_4(){
+  document.getElementById("text_box4").value='';
+}
 
